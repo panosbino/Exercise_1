d5ad11154da28a57b5e58a92813537c3cab08dcc
Panos Kalogeropoulos <panosbino@gmail.com>
2020-09-01 18:03:53 +0200

    First step

---
+++ printtable.py
@@ -0,0 +1,10 @@
+def multtable(start, stop, number):
+    """
+    Print multiplication table for <number>
+    from <start> to including <stop> 
+    """
+    for i in range(start, stop+1):
+        print(f"{i} x {number} = {i*number}")
+
+if __name__ == '__main__':
+    multtable(1, 4, 7)
